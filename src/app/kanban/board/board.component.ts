04971eb00d3e2dbb263199be74d258cb9421ec11
Juan Rodriguez <nushio@gmail.com>
2019-12-28 22:48:16 -0600

    Move Tasks from Board to Board

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,27 +1,51 @@
-import { Component, Input } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { TaskDialogComponent } from '../dialogs/task-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import { BoardService } from '../board.service';
-import { Task } from '../board.model';
+import { Component, Input, OnInit } from "@angular/core";
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { TaskDialogComponent } from "../dialogs/task-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { BoardService } from "../board.service";
+import { Board, Task } from "../board.model";
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  selector: "app-board",
+  templateUrl: "./board.component.html",
+  styleUrls: ["./board.component.scss"]
 })
-export class BoardComponent {
-  @Input() board;
+export class BoardComponent implements OnInit {
+  @Input() board: Board;
+  @Input() connectedTo: Board[];
+  public connectedBoards = [];
 
   taskDrop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
-    this.boardService.updateTasks(this.board.id, this.board.tasks);
+    if (event.container.id !== event.previousContainer.id) {
+      var boards: Board[] = [];
+      var oldBoard: Board;
+      this.connectedTo.forEach(board => {
+        if (event.previousContainer.id === board.id) {
+          oldBoard = board;
+          return;
+        }
+      });
+
+      const task: Task = oldBoard.tasks[event.previousIndex];
+      this.board.tasks.splice(event.currentIndex, 0, task);
+      oldBoard.tasks.splice(event.previousIndex, 1);
+      boards.push(oldBoard);
+      boards.push(this.board);
+      this.boardService.moveTasks(boards);
+    } else {
+      moveItemInArray(
+        this.board.tasks,
+        event.previousIndex,
+        event.currentIndex
+      );
+      this.boardService.updateTasks(this.board.id, this.board.tasks);
+    }
   }
 
   openDialog(task?: Task, idx?: number): void {
-    const newTask = { label: 'purple' };
+    const newTask = { label: "purple" };
     const dialogRef = this.dialog.open(TaskDialogComponent, {
-      width: '500px',
+      width: "500px",
       data: task
         ? { task: { ...task }, isNew: false, boardId: this.board.id, idx }
         : { task: newTask, isNew: true }
@@ -45,6 +69,14 @@ export class BoardComponent {
 
   handleDelete() {
     this.boardService.deleteBoard(this.board.id);
+  }
+
+  ngOnInit() {
+    this.connectedTo.forEach(board => {
+      if (board.id !== this.board.id) {
+        this.connectedBoards.push(board.id);
+      }
+    });
   }
 
   constructor(private boardService: BoardService, private dialog: MatDialog) {}
